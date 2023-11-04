228526e189226ff1f048c7cf1a7802cb88d6ef52
Jeremy Tang <tangjere@usc.edu>
2023-11-03 19:31:02 -0700

    some meetings set up

---
+++ app/src/main/java/com/example/talk2friends/MeetingInfo.java
@@ -0,0 +1,13 @@
+package com.example.talk2friends;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+public class MeetingInfo {
+    public int MeetingId;
+    public String Name;
+    public String Description;
+    public String Location;
+    public String Time;
+    public int CreatorId;
+    public ArrayList<Integer> AttendeeIds;
+}
